b3a8de42c05aa47ff498085380819623f56f5206
ashxjain <ash.1382@gmail.com>
2017-01-26 04:19:30 -0800

    Update ipsla script to make it compatible with both Solaris & Linux

--- README.md
+++ README.md
@@ -1,4 +1,66 @@
-# ipsla
-This is the IP SLA Script README PAGE
+# IPSLA
+IPSLA Script will monitor connection links for its availability. If primary link is down, it will create backup static link, until primary link comes up again.
 
-This script runs on Ubuntu Linux (with api-install) or on Netvisor and ONVL Platforms
+This scripts is compatible with Solaris and Linux platforms.
+
+### Dependencies
+ssmtp package (version 2.64)
+
+### How to use
+Script takes a YML file as an input. Following shows a sample YML file:
+```
+LinkTrackIP1:10.10.10.100
+-switch_host:aquila-ext-43
+-switch_username:network-admin
+-switch_password:test123
+-vrouter_name:test-vr
+-network:10.10.10.0
+-netmask:255.255.255.0
+-gateway:10.10.10.1
+-distance:100
+-custom_message:This is the link to US DC
+LinkTrackIP2:11.11.11.100
+-switch_host:aquila-ext-43
+-switch_username:network-admin
+-switch_password:test123
+-vrouter_name:test-vr
+-network:11.11.11.0
+-netmask:255.255.255.0
+-gateway:11.11.11.1
+-distance:100
+-custom_message:This is the link to Germany DC
+email_id:<EMAIL-ID>
+email_password:<EMAIL-PASSWORD>
+link_down_message:Link is not reachable
+link_up_message:Link is back up
+sysadmin:<SYSADMIN-EMAIL-ADDRESS>
+ping_timer:3
+failure_threshold:2
+keep_backup_route:false
+```
+
+For every link to track specify the following set of items per link:
+```
+LinkTrackIP:Remote IP to track
+-switch_host:Switch from where to track
+-switch_username:Switch username
+-switch_password:Switch password
+-vrouter_name:vRouter from which to send ping
+-network:Network info to add backup route
+-netmask:Netmask info to add backup route
+-gateway:Gateway info to add backup route
+-distance:Distance info to add backup route
+-custom_message:Per link custom message to be added in notification email
+```
+
+And other general configuration items:
+```
+email_id:Email-ID from which to send emails
+email_Password:Email password
+link_down_message:Email message content when a link goes down
+link_up_message:Email message content when a link comes up
+sysadmin:Email-ID to send notifications to
+ping_timer:Number of seconds of delay between each ping
+failure_threshold:Maximum number of ping failures to take, after which send notification
+keep_backup_route:Once link comes backup, flag to remove backup route or not
+```
--- ipsla.sh
+++ ipsla.sh
@@ -1,252 +1,242 @@
 #!/bin/bash
 
 if [[ $( uname -s ) = "SunOS" ]]; then
-	GREP="ggrep"
+        GREP="ggrep"
 else
-	GREP="grep"
+        GREP="grep"
 fi
 
-function error() {
-  echo -e "\e[0;33mERROR: The IPSLA script failed while running the command $BASH_COMMAND at line $BASH_LINENO.\e[0m" >&2
-  exit 1
-}
 trap error ERR
+trap cleanup SIGINT SIGTERM EXIT
 
 RED='\033[0;31m'
 NC='\033[0m'
 
-default_yml_file="ping-type:vrouter
-no_of_links:3
-switch_host:auto-spine1
-switch_username:network-admin
-switch_password:test123
-LinkTrackIP1:172.168.0.2
--vrouter-name:o-spine1-vrouter
--network:172.168.0.0
--netmask:255.255.255.252
--gateway:172.168.0.1
-LinkTrackIP2:172.168.0.10
--vrouter-name:o-spine1-vrouter
--network:172.168.0.8
--netmask:255.255.255.252
--gateway:172.168.0.9
-LinkTrackIP3:172.168.0.6
--vrouter-name:o-spine1-vrouter
--network:172.168.0.4
--netmask:255.255.255.252
--gateway:172.168.0.5
-distance:100
-emailId:testipsla@gmail.com
-emailPassword:testipsla123
-emailContent1:Link is Down with for Address
-emailContent2:Rolling back. Link is up for Address
-sysadmin:sandip.divekar@calsoftinc.com
+default_yml_file="
+LinkTrackIP1:10.10.10.100
+-switch_host:aquila-ext-43
+-switch_username:network-admin
+-switch_password:test123
+-vrouter_name:test-vr
+-network:10.10.10.0
+-netmask:255.255.255.0
+-gateway:10.10.10.1
+-distance:100
+-custom_message:This is the link to US DC
+LinkTrackIP2:11.11.11.100
+-switch_host:aquila-ext-43
+-switch_username:network-admin
+-switch_password:test123
+-vrouter_name:test-vr
+-network:11.11.11.0
+-netmask:255.255.255.0
+-gateway:11.11.11.1
+-distance:100
+-custom_message:This is the link to Germany DC
+email_id:<EMAIL-ID>
+email_password:<EMAIL-PASSWORD>
+link_down_message:Link is not reachable
+link_up_message:Link is back up
+sysadmin:<SYSADMIN-EMAIL-ADDRESS>
 ping_timer:3
-allowed_failure:2
+failure_threshold:2
+keep_backup_route:false
 "
 
 print_help()
 {
-	printf "  IPSLA Script will monitor connection links for its\n"
-	printf "  availability. If primary link is down, it will create\n"
-	printf "  backup static link, until primary link comes up again.\n\n"
+        printf "  IPSLA Script will monitor connection links for its\n"
+        printf "  availability. If primary link is down, it will create\n"
+        printf "  backup static link, until primary link comes up again.\n\n"
 
-	printf "  Usage: ipsla [OPTIONS]\n\n"
-	printf "  OPTIONS:\n"
-	printf "      -f        Read YML file as input. This should contain\n"
-	printf "                details of links to track, host from which\n"
-	printf "                to track, email settings and other configurations\n"
-	printf "      -h        Print this help\n"
+        printf "  Usage: ipsla [OPTIONS]\n\n"
+        printf "  OPTIONS:\n"
+        printf "      -f        Read YML file as input. This should contain\n"
+        printf "                details of links to track, host from which\n"
+        printf "                to track, email settings and other configurations\n"
+        printf "      -h        Print this help\n"
 }
 
 generate_yml()
 {
-	choice="n"
-	printf "Please provide YML file as an argument : ${RED}${0##*/} -f <filename.yml>${NC}\n\n"
-	printf "Do you want to generate sample YML file in /tmp/file.yml\n"
-	printf "as a example. ${RED}(y/n):${NC}"
-	read choice
-	if [ $choice == "y" ]; then
-		echo "$default_yml_file" > /tmp/file.yml
-		printf "\n#####Sample YML file is created in /tmp/file.yml######\n"
-		printf "\n${RED}$default_yml_file${NC}\n\n"
+        choice="n"
+        printf "Please provide YML file as an argument : ${RED}${0##*/} -f <filename.yml>${NC}\n\n"
+        printf "Do you want to generate sample YML file in /tmp/file.yml\n"
+        printf "as a example. ${RED}(y/n):${NC}"
+        read choice
+        if [ $choice == "y" ]; then
+                echo "$default_yml_file" > /tmp/file.yml
+                printf "\n#####Sample YML file is created in /tmp/file.yml######\n"
+                printf "\n${RED}$default_yml_file${NC}\n\n"
+        fi
+        exit 0
+}
+
+error() {
+	log "[ERROR]: The IPSLA script failed while running the command $BASH_COMMAND at line $BASH_LINENO"
+	exit 1
+}
+
+
+cleanup() {
+	if [ -f $LOCKFILE ]; then
+		rm -f "$LOCKFILE"
 	fi
-	exit 0
+	trap - SIGTERM && kill -- -$$
+}
+
+vrouter_ping()
+{
+	cli=$1
+	vrouter=$2
+	ltip=$3
+        $cli vrouter-ping vrouter-name $vrouter host-ip $ltip count 1 | $GREP -E 'Unreachable|unreachable|unknown|not|100% packet loss' | wc -l
+}
+
+configure_ssmtp()
+{
+	email_id=`cat $ymlfile | $GREP 'email_id' | cut -d : -f2`
+	email_password=`cat $ymlfile | $GREP 'email_password' | cut -d : -f2`
+	mkdir -p /etc/ssmtp/
+	touch /etc/ssmtp/ssmtp.conf
+	if ! grep -q "##IPSLA EMAIL CONF##" /etc/ssmtp/ssmtp.conf; then
+		echo "##IPSLA EMAIL CONF##
+AuthUser=$email_id
+AuthPass=$email_password
+FromLineOverride=YES
+mailhub=smtp.gmail.com:587
+UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
+	fi
+}
+
+send_mail()
+{
+        subject=$1
+        content=$2
+	cmsg=$3
+ssmtp -C/etc/ssmtp/ssmtp.conf $sysadmin << EOF
+To: $sysadmin
+From:$email_id
+Subject: $subject
+
+$content
+
+$cmsg
+EOF
+}
+
+LOGFILE="/var/tmp/ipsla.log"
+LOCKFILE="/tmp/.ipsla.lock"
+
+log()
+{
+	if ( set -o noclobber; echo "$$" > "$LOCKFILE") 2> /dev/null; then
+		echo `date +"%b%d.%H.%M.%S:"`"ipsla_notification:$1" >> $LOGFILE
+		rm -f "$LOCKFILE"
+	fi
+}
+
+track_link()
+{
+	cli=$1
+	ltip=$2
+	vrouter=$3
+	network=$4
+	netmask=$5
+	gateway=$6
+	distance=$7
+	cmsg=$8
+        ping_failure=0
+	while true
+        do
+                count=`vrouter_ping "$cli" $vrouter $ltip`
+
+		# Check for ping failure
+                if [ $count -ne 0 ]; then
+                        ping_failure=$((ping_failure+1))
+                        if [[ ! $ping_failure > $failure_threshold ]]; then
+				log "[$ltip] LinkTrackIP is unreachable"
+			fi
+			if [ $ping_failure == $failure_threshold ]; then
+				log "[$ltip] Notify sysadmin:$sysadmin - LinkTrackIP is unreachable"
+				send_mail "IPSLA-Notification: LinkTrackIP $ltip is unreachable from vrouter $vrouter" "$link_down_message" "$cmsg"
+				log "[$ltip] Adding backup route for network: $network"
+				$cli vrouter-static-route-add vrouter-name $vrouter network $network netmask $netmask gateway-ip $gateway distance $distance
+			fi
+		else
+                        if [[ ! $ping_failure < $failure_threshold ]]; then
+				log "[$ltip] Notify sysadmin:$sysadmin - LinkTrackIP is reachable"
+				if [ "$keep_backup_route" != "true" ]; then
+					log "[$ltip] Removing backup route for network: $network"
+					$cli vrouter-static-route-remove vrouter-name $vrouter network $network netmask $netmask gateway-ip $gateway
+				fi
+				send_mail "IPSLA-Notification: LinkTrackIP $ltip is reachable from vrouter $vrouter" "$link_up_message" "$cmsg"
+			fi	
+			ping_failure=0
+                fi
+		sleep $ping_timer
+	done
 }
 
 if [[ ! $@ =~ ^\-.+ ]]
 then
-	generate_yml
+        generate_yml
 fi
 while getopts ":f:h:" opt; do
-	case $opt in
-	f)
-		if [ ! -f $OPTARG ]; then
-			echo "$OPTARG does not exist !"
-			exit 1
-		fi
-		ymlfile=$OPTARG
-	;;
-	h)
-		print_help
-		exit 0
-	;;
-	\?)
-		echo "Invalid option: -$OPTARG" >&2
-		print_help
-		exit 1
-	;;
-	:)
-		print_help
-		exit 1
-	;;
-	esac
+        case $opt in
+        f)
+                if [ ! -f $OPTARG ]; then
+                        echo "$OPTARG does not exist !"
+                        exit 1
+                fi
+                ymlfile=$OPTARG
+        ;;
+        h)
+                print_help
+                exit 0
+        ;;
+        \?)
+                echo "Invalid option: -$OPTARG" >&2
+                print_help
+                exit 1
+        ;;
+        :)
+                print_help
+                exit 1
+        ;;
+        esac
 done
 
 # Get input details from YML file
 
 ping_timer=`cat $ymlfile | $GREP 'ping_timer' | cut -d : -f2`
-allowed_failure=`cat $ymlfile | $GREP 'allowed_failure' | cut -d : -f2`
-emailContent1=`cat $ymlfile | $GREP 'emailContent1' | cut -d : -f2`
-emailContent2=`cat $ymlfile | $GREP 'emailContent2' | cut -d : -f2`
+failure_threshold=`cat $ymlfile | $GREP 'failure_threshold' | cut -d : -f2`
+link_up_message=`cat $ymlfile | $GREP 'link_up_message' | cut -d : -f2`
+link_down_message=`cat $ymlfile | $GREP 'link_down_message' | cut -d : -f2`
 sysadmin=`cat $ymlfile | $GREP 'sysadmin' | cut -d : -f2`
-switch_host=`cat $ymlfile | $GREP 'switch_host' | cut -d : -f2`
-switch_username=`cat $ymlfile | $GREP 'switch_username' | cut -d : -f2`
-switch_password=`cat $ymlfile | $GREP 'switch_password' | cut -d : -f2`
+keep_backup_route=`cat $ymlfile | $GREP 'keep_backup_route' | cut -d : -f2`
 
-links=`cat $ymlfile | $GREP 'no_of_links' | cut -d : -f2`
+links=`cat $ymlfile | grep "LinkTrackIP" | wc -l`
 
-i=1
+configure_ssmtp
 
-hosts=()
-vrouters=()
-networks=()
-netmasks=()
-gateways=()
+for (( i=1; i<=$links; i++)) do
+        linktrackip=`cat $ymlfile | $GREP "^LinkTrackIP$i" | cut -d : -f2`
+        switch_host=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-switch_host" | cut -d : -f2`
+        switch_username=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-switch_username" | cut -d : -f2`
+        switch_password=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-switch_password" | cut -d : -f2`
+        vrouter=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-vrouter_name" | cut -d : -f2`
+        network=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-network" | cut -d : -f2`
+        netmask=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-netmask" | cut -d : -f2`
+        gateway=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-gateway" | cut -d : -f2`
+        distance=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-distance" | cut -d : -f2`
+        cmsg=`cat $ymlfile | $GREP -A 9 "^LinkTrackIP$i" | $GREP "^-custom_message" | cut -d : -f2`
+	if [ $switch_host == `hostname` ]; then
+		cli="cli --user $switch_username:$switch_password --quiet "
+	else
+		cli="cli --user $switch_username:$switch_password --quiet --host $switch_host "
+	fi
 
-for((i=1;i<=$links;i++))
-do
-  hostname=`cat $ymlfile | $GREP "LinkTrackIP$i" | cut -d : -f2`
-  vrouter=`cat $ymlfile | $GREP -A 4 "LinkTrackIP$i" | $GREP "vrouter-name" | cut -d : -f2`
-  network=`cat $ymlfile | $GREP -A 4 "LinkTrackIP$i" | $GREP "network" | cut -d : -f2`
-  netmask=`cat $ymlfile | $GREP -A 4 "LinkTrackIP$i" | $GREP "netmask" | cut -d : -f2`
-  gateway=`cat $ymlfile | $GREP -A 4 "LinkTrackIP$i" | $GREP "gateway" | cut -d : -f2`
-  hosts+=($hostname)
-  vrouters+=($vrouter)
-  networks+=($network)
-  netmasks+=($netmask)
-  gateways+=($gateway) 
+        track_link "$cli" $linktrackip $vrouter $network $netmask $gateway $distance "$cmsg" &
 done
-distance=`cat $ymlfile | $GREP 'distance' | cut -d : -f2`
-
-
-create_static_route()
-{
-  echo "Creating static route"
-  index=$1
-  cli --user $switch_username:$switch_password --quiet --host $switch_host vrouter-static-route-add vrouter-name ${vrouters[$index]} network ${networks[$index]} netmask ${netmasks[$index]} gateway-ip ${gateways[$index]} distance $distance
-  echo "Created static route for network:${networks[$index]}"
-}
-
-delete_static_route()
-{
-  index=$1
-  echo "Deleting static route for network: ${networks[$index]}"
-  cli --user $switch_username:$switch_password --quiet --host $switch_host vrouter-static-route-remove vrouter-name ${vrouters[$index]} network ${networks[$index]} netmask ${netmasks[$index]} gateway-ip ${gateways[$index]}
-}
-
-stop_exit=0
-reset=5
-
-ping_host()
-{
-  index=$1
-  ping_failure=0
-  reset=0
-  while [ "$ping_failure" -ne "$allowed_failure" ];
-  do
-    index_for_old_link_check=$index
-    echo "Pinging host - ${hosts[$index]}"
-    count=`cli --user $switch_username:$switch_password --quiet --host $switch_host vrouter-ping vrouter-name ${vrouters[$index]} host-ip ${hosts[$index]} count 1 | $GREP -E 'Unreachable|unreachable|unknown|not' | wc -l`
-    if ! [ $count == 0 ];
-    then
-      ping_failure=$((ping_failure+1))
-      echo "Host is unrechable:${hosts[$index]}"
-    fi
-    for((;$index_for_old_link_check>0;))
-    do
-      index_for_old_link_check=$((index_for_old_link_check-1))
-      count=`cli --user $switch_username:$switch_password --quiet --host $switch_host vrouter-ping vrouter-name ${vrouters[$index_for_old_link_check]} host-ip ${hosts[$index_for_old_link_check]} count 1 | $GREP -E 'Unreachable|unreachable|unknown|not' | wc -l`
-
-      if [ $count == 0 ];
-      then
-        diff=`expr $index - $index_for_old_link_check`
-        j=$index
-        for ((;$diff!=0;diff--))
-        do
-          delete_static_route $j
-          j=$((j-1))
-        done
-        stop_exit=$index_for_old_link_check
-        reset=1
-        echo "$emailContent2 : ${hosts[$index_for_old_link_check]}" | mail -s "Link ${hosts[$index_for_old_link_check]} is down" $sysadmin
-        return 0
-      fi
-
-    done
-
-    echo "Sleeping for 3 seconds"
-    sleep $ping_timer
-  done
-  echo "Host ${hosts[$index]} is down. Sending Email"
-  echo "$emailContent1 : ${hosts[$index]}" | mail -s "Link ${hosts[$index]} is down" $sysadmin
-  stop_exit=$index
-  reset=0
-}
-
-#install_ssmtp
-
-install_ssmtp()
-{
-  sudo apt-get -y -qq update
-  sudo apt-get -y -qq autoremove sendmail
-  sudo apt-get -y -qq install ssmtp
-  sudo apt-get -y -qq install mailutils
-  emailid=`cat $ymlfile | $GREP 'emailId' | cut -d : -f2`
-  emailPassword=`cat $ymlfile | $GREP 'emailPassword' | cut -d : -f2`
-  count=`cat /etc/ssmtp/ssmtp.conf | $GREP '##EMAIL CONF##' | wc -l`
-  if [ $count == "0" ]; then
-  echo "##EMAIL CONF##
-AuthUser=$emailid
-AuthPass=$emailPassword
-FromLineOverride=YES
-mailhub=smtp.gmail.com:587
-UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
-  fi
-}
-
-i=1
-for((;i<=$links;i++))
-do
-  index=$(($i-1))
-  ping_host $index
-  temp=$stop_exit
-  if [ $reset == 1  ];
-  then
-    i=$temp
-  fi
-
-  if [ $i == $links ];
-  then
-    echo "All links are down exiting from Script"
-    exit 0
-  else
-    if [ $reset != 1 ];
-    then
-      j=$(($temp+1))
-      create_static_route $j
-    fi
-  fi
-
-done
-
+wait
